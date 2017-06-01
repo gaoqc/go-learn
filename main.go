181856e945afcb7cdb7e979dd2237724a4563213
gaoqc <gaoqicheng023@pingan.com.cn>
2017-06-01 09:40:32 +0800

    about template

--- main.go
+++ main.go
@@ -1,46 +1,47 @@
 package main
 
 import (
-	// "encoding/xml"
 	"fmt"
-	// "io/ioutil"
-	"net/http"
-	// "os"
-	"regexp"
-	"strings"
+	"html/template"
+	"os"
 )
 
-func print(resp http.ResponseWriter, req *http.Request) {
-	req.ParseForm()
-	fmt.Println(req.Form)
-	fmt.Println("path", req.URL.Path)
-	fmt.Println("schem", req.URL.Scheme)
-	fmt.Println(req.Form["url_long"])
-	for k, v := range req.Form {
-		fmt.Println("key:", k, ",val:", strings.Join(v, ""))
-	}
-	fmt.Fprintln(resp, "hello gaoqc")
-}
-
-type Servers struct {
-	// XMLName     xml.Name `xml:"servers"`
-	Version     string   `xml:"version,attr"`
-	Svs         []Server `xml:"server"`
-	Description string   `xml:",innerxml"`
-}
-type Server struct {
-	// XMLName    xmldd.Name `xml:"server"`
-	ServerName string `xml:"serverName"`
-	ServerIp   string `xml:"serverIp"`
+type ClzInfo struct {
+	ClzName  string
+	InstName string
+	Desc     string
 }
 
 func main() {
-	str := "I am learning Go language"
-	re, err := regexp.Compile("lang.*")
+	path := "/Users/gaoqc/Documents/codes/pingan/doc/tmp/tpl"
+	tpl := template.Must(template.ParseFiles(path+"/controller/TplController.java", path+"/controller/TplESAController.java", path+"/service/TplService.java", path+"/service/TplServiceImpl.java"))
+	clzInfo := ClzInfo{"Test", "test", "just for test"}
+	// tpl.ExecuteTemplate(os.Stdout, "tplESAController", clzInfo)
+	// tpl.ExecuteTemplate(os.Stdout, "tplService", clzInfo)
+	// tpl.ExecuteTemplate(os.Stdout, "tplServiceImpl", clzInfo)
+
+	destPath := "/Users/gaoqc/Documents/codes/pingan/doc/tmp/tpl/dest/"
+
+	// tplMap := make(map[string]string)
+	// tplMap["tplController"] = destPath + clzInfo.ClzName + "Controller.java"
+	// tplMap["tplESAController"] = destPath + clzInfo.ClzName + "Controller.java"
+	// tplMap["tplSerivce"] = destPath + clzInfo.ClzName + "Service.java"
+	// tplMap["tplServiceImpl"] = destPath + clzInfo.ClzName + "ServiceImpl.java"
+	// for k, v := range tplMap {
+	// 	tpl.ExecuteTemplate(getFile(destPath+clzInfo.ClzName+"Controller.java"), "tplController", clzInfo)
+	// 	// fmt.Println("k=%s ,v=%s", k, v)
+	// }
+	tpl.ExecuteTemplate(getFile(destPath+clzInfo.ClzName+"Controller.java"), "tplController", clzInfo)
+	tpl.ExecuteTemplate(getFile(destPath+clzInfo.ClzName+"ESAController.java"), "tplESAController", clzInfo)
+	tpl.ExecuteTemplate(getFile(destPath+clzInfo.ClzName+"Service.java"), "tplService", clzInfo)
+	tpl.ExecuteTemplate(getFile(destPath+clzInfo.ClzName+"ServiceImpl.java"), "tplServiceImpl", clzInfo)
+}
+func getFile(filepath string) *os.File {
+	f, err := os.Create(filepath)
 	if err != nil {
-		fmt.Printf("err:%v", err)
-		return
+		fmt.Println("err:%v", err)
+		return nil
 	}
-	fmt.Println("orgin str:", str)
-	fmt.Println("replace:", re.ReplaceAll(str, []byte("1111")))
+	return f
+
 }
